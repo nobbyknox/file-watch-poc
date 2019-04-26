f538fc6fd58f911d376065cdeb327ef8a2785cb0
Nobby Knox <nobbyknox@gmail.com>
2019-04-26 19:57:38 +0200

    Give "Try" a try during initialization

--- lion/src/main/scala/com/nobbyknox/Application.scala
+++ lion/src/main/scala/com/nobbyknox/Application.scala
@@ -8,6 +8,8 @@ import grizzled.slf4j.Logger
 import org.apache.commons.cli.{DefaultParser, HelpFormatter, Options}
 import org.apache.log4j.{Level => Level4J, Logger => Logger4J}
 
+import scala.util.{Failure, Success, Try}
+
 object Application extends App {
 
   val logger = Logger("Application")
@@ -17,6 +19,7 @@ object Application extends App {
 
   val commandLineOptions = getCommandLineOptions
   val commandLineArguments = getUserCommandLineArguments(commandLineOptions)
+  var context: AppContext = _
 
   // If help requested or properties not specified, print help and exit
   if (commandLineArguments.hasOption("h") || !commandLineArguments.hasOption("p")) {
@@ -25,8 +28,16 @@ object Application extends App {
     sys.exit(0)
   }
 
-  // TODO: Rewrite this using Try/Success/Failure
-  val context = initializeApp
+  // https://alvinalexander.com/scala/functional-error-handling-exceptions-in-scala
+  // https://alvinalexander.com/source-code/scala/scala-try-success-and-failure-example
+  initializeApp match {
+    case Success(c) => context = c
+    case Failure(exception) => {
+      logger.error(exception)
+      sys.exit(1)
+    }
+  }
+
   val watcher = Watcher(context)
 
   context.databaseManager.start()
@@ -50,27 +61,16 @@ object Application extends App {
     logger.info("Goodbye")
   })
 
-  def initializeApp: AppContext = {
+  def initializeApp: Try[AppContext] = Try {
     val props = getProperties
     val dm = getDatabaseManager(props)
     new AppContext(props, dm)
   }
 
   def getProperties: Properties = {
-    try {
-      val properties = new Properties()
-      properties.load(new FileInputStream(commandLineArguments.getOptionValue("p")))
-      properties
-    } catch {
-      case e: FileNotFoundException => {
-        logger.error(s"The properties file ${commandLineArguments.getOptionValue("p")} does not exist")
-        sys.exit(1)
-      }
-      case e => {
-        println(e.printStackTrace())
-        sys.exit(1)
-      }
-    }
+    val properties = new Properties()
+    properties.load(new FileInputStream(commandLineArguments.getOptionValue("p")))
+    properties
   }
 
   def getDatabaseManager(properties: Properties): DatabaseManager = {
